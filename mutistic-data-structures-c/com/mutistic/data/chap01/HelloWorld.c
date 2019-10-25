a5a55f4e4ea226b2e0bb474d5f5c5349ac5ca781
mutistic-client <mutistic@qq.com>
2019-10-25 19:13:38 +0800

    v1.0.0-初始化mutisitic-data-structures-c项目

---
+++ mutistic-data-structures-c/com/mutistic/data/chap01/HelloWorld.c
@@ -0,0 +1,11 @@
+#include<stdio.h> // 导入stdio.h
+#include<stdlib.h> // 导入stdlib.h
+
+// main主函数
+int main(void){
+    // 打印函数
+    printf("Hello World!!!!\n");
+    // 使程序暂停
+    system("pause");
+    return 0;
+}
