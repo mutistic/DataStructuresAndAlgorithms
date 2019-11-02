e5e4482fcd38e5325d6cdcbfbb80e2d62ae60755
mutistic <mutistic@qq.com>
2019-11-02 23:59:36 +0800

    v0.0.4.2-O(logn)对数阶的推导

---
+++ data-structures-java/src/algorithm/chap01/GaussAlgorithm.java
@@ -0,0 +1,38 @@
+package algorithm.chap01;
+
+import java.util.Scanner;
+
+/**
+ * 算法：高斯算法，Gauss Algorithm
+ * <p> 描述：以首项加末项乘以项数除以2用来计算“1+2+3+4+5+···+(n-1)+n”的结果
+ * <p> 公式：项数 * (首项 + 末项) / 2
+ * <p> 时间复杂度：O(1)，常数阶
+ * <p> 类型：等差数列求和算法
+ * <p> 等差数列求和公式：Sn = (a1+an)n / 2，a1∈N*，n<∈N*。有且仅有当a1=1，公差d=1时，an=n
+ */
+public class GaussAlgorithm {
+    // 高斯算法的实现
+    public static long gaussAlgorithm(long n) {
+        return n * (1 + n) / 2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("\n=================================");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("计算Sn=1+2+3+4+5+···+(n-1)+n的结果，请输入正自然数n的值：");
+        long n = 0;
+        try {
+            n = scan.nextLong();
+        } catch (Exception e) {
+            System.err.println("输入的正自然数n不合法");
+            return;
+        }
+        if (n < 1) {
+            System.err.println("输入的正自然数n必须大于1");
+            return;
+        }
+        long sum = gaussAlgorithm(n);
+        System.out.println("Sn=1+2+3+4+5+···+" + (n - 1) + "+" + n + "=" + sum);
+    }
+
+}
---
+++ data-structures-java/src/chap01/complexity/LogarithmTime.java
@@ -0,0 +1,30 @@
+package chap01.complexity;
+
+/**
+ * 对数阶O(logn)的推导过程
+ * <p> 算法时间复杂度
+ */
+public class LogarithmTime {
+    public static void main(String[] args) {
+        f_RightHalfChars("ABCDEFGHIJKLMNOPKRSTUVWXYZ", 1);
+    }
+
+    // 算法f(RightHalfChars)=递归地将字符串对半并且输出右边字符
+    public static void f_RightHalfChars(String chars, int execNum) {
+        if (chars == null || chars.length() == 0) {
+            return;
+        }
+
+        String rightChars = "";
+        int rightLength = chars.length() / 2;
+        if (rightLength > 0) {
+            rightChars = chars.substring(rightLength, chars.length());
+            System.out.println("第" + execNum + "次执行输出结果：" + rightChars);
+            f_RightHalfChars(rightChars, execNum + 1);
+        }
+        // --- 递归结束条件：字符串chars为空或空串，递归执行条件：字符串chars长度对半取最小整数后大于0
+        // --- 随着字符串长度(即问题规模n)的扩大，由于每次输出之前都将字符串对半
+        // --- 则算法f(RightHalfChars)的执行次数的平方也趋近于n，则可推导出算法f(a)的时间复杂度为 O(log2n)，
+        // --- 由于log2n的底为常数2，则在大O阶的推导中被忽略，所以认为：T(n) = O(logn)
+    }
+}
